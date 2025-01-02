c0883303266d628a2b189c670a5dc023ce16c1bc
DenzelGitonga007 <gitongadenzel@gmail.com>
2025-01-02 14:47:36 +0300

    text

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.firstapp.ui.theme.FirstAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +29,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FirstAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+            Demo()
+
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Demo(){
+    Column(modifier = Modifier.fillMaxSize().background(Color.LightGray)){
+        Text(
+            "Denzel",
+            fontSize = 30.sp,
+            color = Color.Red,
+            fontFamily = FontFamily.SansSerif,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold,
+            textDecoration = TextDecoration.Underline,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+            )
+
+        Text("Gitonga")
+        Text("Gitonga")
+        Text("Gitonga")
+        Text("Gitonga")
+    }
+
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FirstAppTheme {
-        Greeting("Android")
-    }
+fun DemoPreview(){
+
+    Demo()
 }
